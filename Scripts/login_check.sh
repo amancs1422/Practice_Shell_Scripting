99c998d6ec6ed2687042fe11b1776423db2e6551
Aman Kumar <32352507+amancs1422@users.noreply.github.com>
2025-08-01 23:34:28 +0530

    Add files via upload

---
+++ Scripts/login_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Script Name: login_check.sh
+# Description: This script does the first preliminary checks once we logon to the terminal.
+# Author: Aman Kumar
+# Date: 2025-07-29
+# Version: 1.0
+#
+echo "Current Date and Time : $(date)"
+echo "Current User : $(whoami)"
+echo "Present Working Directory : $(pwd)"
+echo "Current filesystem Utilization : $(df -hT)"
+#
+# Signing off: AK
+#
