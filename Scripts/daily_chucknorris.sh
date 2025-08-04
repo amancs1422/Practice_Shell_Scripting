5aa1d5e69f8ecc50165830734b1494c72f11457a
amancs1422 <amancs1422@gmail.com>
2025-08-04 23:33:50 +0530

    Update daily_chucknorris.sh

--- Scripts/daily_chucknorris.sh
+++ Scripts/daily_chucknorris.sh
@@ -1,3 +1,10 @@
+#
+# Script Name: login_check.sh
+# Description: This script does the first preliminary checks once we logon to the terminal.
+# Author: Aman Kumar
+# Date: 2025-08-01
+# Version: 1.0
+#
 #!/bin/bash
 curl -H "Accept: text/plain" https://api.chucknorris.io/jokes/random 
 echo -e "\n"
