da92be75dab114fcf66f9ecdefe54485e9b60ccd
amancs1422 <amancs1422@gmail.com>
2025-08-04 23:32:39 +0530

    Update joke_rolodex.sh

--- Scripts/joke_rolodex.sh
+++ Scripts/joke_rolodex.sh
@@ -1,3 +1,10 @@
+#
+# Script Name: login_check.sh
+# Description: This script does the first preliminary checks once we logon to the terminal.
+# Author: Aman Kumar
+# Date: 2025-07-30
+# Version: 1.0
+#
 #!/bin/bash
 curl -H "Accept: text/plain" https://icanhazdadjoke.com/ 
 echo -e "\n"
