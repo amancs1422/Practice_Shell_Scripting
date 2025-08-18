90857603cb87b1ac0ca0581d32a781db3a29c405
amancs1422 <amancs1422@gmail.com>
2025-08-18 13:54:03 +0530

    Update basic_calc.sh

--- Scripts/basic_calc.sh
+++ Scripts/basic_calc.sh
@@ -1,3 +1,10 @@
+#
+# Script Name: login_check.sh
+# Description: This script performs basic arithmetic addition on user provided numbers. 
+# Author: Aman Kumar
+# Date: 2025-07-30
+# Version: 1.0
+#
 #!/bin/bash
 read -p "Enter the operation you want to perform (+,-,/,*) : " op
 read -p "Enter the first number : " num1
