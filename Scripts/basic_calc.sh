4c50c27b07d5f6a7af2d54a89ccd6115ab4ef7ff
amancs1422 <amancs1422@gmail.com>
2025-08-18 21:41:43 +0530

    Update basic_calc.sh

--- Scripts/basic_calc.sh
+++ Scripts/basic_calc.sh
@@ -10,9 +10,9 @@ read -p "Enter the operation you want to perform (+,-,/,*) : " op
 read -p "Enter the first number : " num1
 read -p "Enter the second number : " num2
 # Use bc for floating-point support
+# You can download 
 result=$(echo "$num1 $op $num2" | bc -l)
 echo "The result of $num1 $op $num2 is : $result"
-#
 #
 # Signing off: Aman Kumar
 #
